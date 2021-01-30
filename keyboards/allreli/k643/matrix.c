1b6c020b892a54dc23a8d534188edfeaeb6f017a
Florian Labarre <florian.labarre@siatech.fr>
2021-01-30 17:35:15 +0100

    Use PWM to read the matrix

--- keyboards/allreli/k643/matrix.c
+++ keyboards/allreli/k643/matrix.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2012-2018 Jun Wako, Jack Humbert, Yiancar
+Copyright 2011 Jun Wako <wakojun@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -13,112 +13,50 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Ported to QMK by Stephen Peery <https://github.com/smp4488/>
 */
+
 #include <stdint.h>
 #include <stdbool.h>
+#include <SN32F240B.h>
+#include "ch.h"
+#include "hal.h"
+#include "CT16.h"
+
+#include "wait.h"
 #include "util.h"
 #include "matrix.h"
 #include "debounce.h"
 #include "quantum.h"
 
-#ifdef DIRECT_PINS
-static pin_t direct_pins[MATRIX_ROWS][MATRIX_COLS] = DIRECT_PINS;
-#elif (DIODE_DIRECTION == ROW2COL) || (DIODE_DIRECTION == COL2ROW)
 static const pin_t row_pins[MATRIX_ROWS] = MATRIX_ROW_PINS;
 static const pin_t col_pins[MATRIX_COLS] = MATRIX_COL_PINS;
-static const pin_t led_row_pins[LED_MATRIX_ROWS_HW] = LED_MATRIX_ROW_PINS;
-#endif
+//static const pin_t led_row_pins[LED_MATRIX_ROWS_HW] = LED_MATRIX_ROW_PINS;
+//static uint16_t row_ofsts[LED_MATRIX_ROWS];
 
-/* matrix state(1:on, 0:off) */
-extern matrix_row_t raw_matrix[MATRIX_ROWS];  // raw values
-extern matrix_row_t matrix[MATRIX_ROWS];      // debounced values
+matrix_row_t raw_matrix[MATRIX_ROWS]; //raw values
+matrix_row_t last_matrix[MATRIX_ROWS] = {0};  // raw values
+matrix_row_t matrix[MATRIX_ROWS]; //debounced values
 
-// matrix code
+static bool matrix_changed = false;
+static uint8_t current_col = 0;
+//static uint8_t current_row = 0;
+static uint8_t current_led_row =0;
 
-#ifdef DIRECT_PINS
+//extern volatile LED_TYPE led_state[DRIVER_LED_TOTAL];
 
-static void init_pins(void) {
-    for (int row = 0; row < MATRIX_ROWS; row++) {
-        for (int col = 0; col < MATRIX_COLS; col++) {
-            pin_t pin = direct_pins[row][col];
-            if (pin != NO_PIN) {
-                setPinInputHigh(pin);
-            }
-        }
-    }
-}
+__attribute__((weak)) void matrix_init_kb(void) { matrix_init_user(); }
 
-static bool read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row) {
-    // Start with a clear matrix row
-    matrix_row_t current_row_value = 0;
+__attribute__((weak)) void matrix_scan_kb(void) { matrix_scan_user(); }
 
-    for (uint8_t col_index = 0; col_index < MATRIX_COLS; col_index++) {
-        pin_t pin = direct_pins[current_row][col_index];
-        if (pin != NO_PIN) {
-            current_row_value |= readPin(pin) ? 0 : (MATRIX_ROW_SHIFTER << col_index);
-        }
-    }
+__attribute__((weak)) void matrix_init_user(void) {}
 
-    // If the row has changed, store the row and return the changed flag.
-    if (current_matrix[current_row] != current_row_value) {
-        current_matrix[current_row] = current_row_value;
-        return true;
-    }
-    return false;
-}
+__attribute__((weak)) void matrix_scan_user(void) {}
 
-#elif defined(DIODE_DIRECTION)
-#    if (DIODE_DIRECTION == COL2ROW)
+inline matrix_row_t matrix_get_row(uint8_t row) { return matrix[row]; }
 
-static void select_row(uint8_t row) {
-    setPinOutput(row_pins[row]);
-    writePinLow(row_pins[row]);
-}
-
-static void unselect_row(uint8_t row) { setPinInputHigh(row_pins[row]); }
-
-static void unselect_rows(void) {
-    for (uint8_t x = 0; x < MATRIX_ROWS; x++) {
-        setPinInputHigh(row_pins[x]);
-    }
-}
-
-static void init_pins(void) {
-    unselect_rows();
-    for (uint8_t x = 0; x < MATRIX_COLS; x++) {
-        setPinInputHigh(col_pins[x]);
-    }
-}
-
-static bool read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row) {
-    // Start with a clear matrix row
-    matrix_row_t current_row_value = 0;
-
-    // Select row and wait for row selecton to stabilize
-    select_row(current_row);
-    matrix_io_delay();
-
-    // For each col...
-    for (uint8_t col_index = 0; col_index < MATRIX_COLS; col_index++) {
-        // Select the col pin to read (active low)
-        uint8_t pin_state = readPin(col_pins[col_index]);
-
-        // Populate the matrix row with the state of the col pin
-        current_row_value |= pin_state ? 0 : (MATRIX_ROW_SHIFTER << col_index);
-    }
-
-    // Unselect row
-    unselect_row(current_row);
-
-    // If the row has changed, store the row and return the changed flag.
-    if (current_matrix[current_row] != current_row_value) {
-        current_matrix[current_row] = current_row_value;
-        return true;
-    }
-    return false;
-}
-
-#    elif (DIODE_DIRECTION == ROW2COL)
+void matrix_print(void) {}
 
 static void select_col(uint8_t col) {
     setPinOutput(col_pins[col]);
@@ -133,54 +71,33 @@ static void unselect_cols(void) {
     }
 }
 
+//static void init_pins(void) {
+//
+//    //  Unselect ROWs
+//    for (uint8_t x = 0; x < MATRIX_ROWS; x++) {
+//        setPinOutput(row_pins[x]);
+//        writePinHigh(row_pins[x]);
+//    }
+//
+//    // Unselect COLs
+//    for (uint8_t x = 0; x < MATRIX_COLS; x++) {
+//        setPinInput(col_pins[x]);
+//        writePinHigh(col_pins[x]);
+//    }
+//
+////    for (uint8_t x = 0; x < LED_MATRIX_ROWS_HW; x++) {
+////        setPinOutput(led_row_pins[x]);
+////        writePinHigh(led_row_pins[x]);
+////    }
+//}
+
+
 static void init_pins(void) {
     unselect_cols();
     for (uint8_t x = 0; x < MATRIX_ROWS; x++) {
         setPinInputHigh(row_pins[x]);
     }
 }
-
-static bool read_rows_on_col(matrix_row_t current_matrix[], uint8_t current_col) {
-    bool matrix_changed = false;
-
-    // Select col and wait for col selecton to stabilize
-    select_col(current_col);
-    matrix_io_delay();
-
-    // For each row...
-    for (uint8_t row_index = 0; row_index < MATRIX_ROWS; row_index++) {
-        // Store last value of row prior to reading
-        matrix_row_t last_row_value    = current_matrix[row_index];
-        matrix_row_t current_row_value = last_row_value;
-
-        // Check row pin state
-        if (readPin(row_pins[row_index]) == 0) {
-            // Pin LO, set col bit
-            current_row_value |= (MATRIX_ROW_SHIFTER << current_col);
-        } else {
-            // Pin HI, clear col bit
-            current_row_value &= ~(MATRIX_ROW_SHIFTER << current_col);
-        }
-
-        // Determine if the matrix changed state
-        if ((last_row_value != current_row_value)) {
-            matrix_changed |= true;
-            current_matrix[row_index] = current_row_value;
-        }
-    }
-
-    // Unselect col
-    unselect_col(current_col);
-
-    return matrix_changed;
-}
-
-#    else
-#        error DIODE_DIRECTION must be one of COL2ROW or ROW2COL!
-#    endif
-#else
-#    error DIODE_DIRECTION is not defined!
-#endif
 
 void matrix_init(void) {
     // initialize key pins
@@ -192,64 +109,244 @@ void matrix_init(void) {
         matrix[i]     = 0;
     }
 
+//    for (uint8_t i = 0; i < LED_MATRIX_ROWS; i++) {
+//        row_ofsts[i] = i * LED_MATRIX_COLS;
+//    }
+
     debounce_init(MATRIX_ROWS);
 
-    {
-        for (uint8_t col_index = 0; col_index < MATRIX_COLS; col_index++)
-        {
-            // Turn COL On
-            setPinOutput(col_pins[col_index]);
-            writePinLow(col_pins[col_index]);
-//        chThdSleepMilliseconds(10);
-
-            for (uint8_t y = 0; y < LED_MATRIX_ROWS_HW; y+=3)
-            {
-                // On
-                // setPinInput(led_row_pins[y]);
-                setPinOutput(led_row_pins[y]);
-                writePinHigh(led_row_pins[y]);
-                chThdSleepMilliseconds(5);
-//        }
-//
-//        ;
-//
-//        chThdSleepMilliseconds(100);
-//
-//        for (uint8_t y = 0; y < LED_MATRIX_ROWS_HW; y+=3)
-//        {
-                // Off
-                // setPinOutput(led_row_pins[y]);
-//            writePinLow(led_row_pins[y]);
-//            chThdSleepMilliseconds(1000);
-            }
-
-            // Turn COL Off
-            setPinInput(col_pins[col_index]);
-            writePinHigh(col_pins[col_index]);
-//        chThdSleepMilliseconds(10);
-        }
-    }
-
     matrix_init_quantum();
+
+    // Enable Timer Clock
+    SN_SYS1->AHBCLKEN_b.CT16B1CLKEN = 1;
+
+    // PFPA - Map most PWM outputs to their PWM A pins
+    SN_PFPA->CT16B1 = 0x00000007;
+
+//    // Enable PWM function, IOs and select the PWM modes
+//    // Enable PWM0-PWM3, PWM8-23
+//    SN_CT16B1->PWMENB   =   (mskCT16_PWM0EN_EN  \
+//                            |mskCT16_PWM1EN_EN  \
+//                            |mskCT16_PWM2EN_EN  \
+//                            |mskCT16_PWM8EN_EN  \
+//                            |mskCT16_PWM9EN_EN  \
+//                            |mskCT16_PWM10EN_EN \
+//                            |mskCT16_PWM11EN_EN \
+//                            |mskCT16_PWM12EN_EN \
+//                            |mskCT16_PWM13EN_EN \
+//                            |mskCT16_PWM14EN_EN \
+//                            |mskCT16_PWM15EN_EN \
+//                            |mskCT16_PWM16EN_EN \
+//                            |mskCT16_PWM17EN_EN \
+//                            |mskCT16_PWM18EN_EN \
+//                            |mskCT16_PWM19EN_EN \
+//                            |mskCT16_PWM20EN_EN \
+//                            |mskCT16_PWM21EN_EN \
+//                            |mskCT16_PWM22EN_EN \
+//                            |mskCT16_PWM23EN_EN);
+//
+//    // Enable PWM0-PWM3, PWM8-PWM23 IO
+//    SN_CT16B1->PWMIOENB   = (mskCT16_PWM0EN_EN  \
+//                            |mskCT16_PWM1EN_EN  \
+//                            |mskCT16_PWM2EN_EN  \
+//                            |mskCT16_PWM8EN_EN  \
+//                            |mskCT16_PWM9EN_EN  \
+//                            |mskCT16_PWM10EN_EN \
+//                            |mskCT16_PWM11EN_EN \
+//                            |mskCT16_PWM12EN_EN \
+//                            |mskCT16_PWM13EN_EN \
+//                            |mskCT16_PWM14EN_EN \
+//                            |mskCT16_PWM15EN_EN \
+//                            |mskCT16_PWM16EN_EN \
+//                            |mskCT16_PWM17EN_EN \
+//                            |mskCT16_PWM18EN_EN \
+//                            |mskCT16_PWM19EN_EN \
+//                            |mskCT16_PWM20EN_EN \
+//                            |mskCT16_PWM21EN_EN \
+//                            |mskCT16_PWM22EN_EN \
+//                            |mskCT16_PWM23EN_EN);
+
+    // Set match interrupts and TC rest
+    SN_CT16B1->MCTRL = (mskCT16_MR3IE_EN|mskCT16_MR3RST_EN);
+
+    // COL match register
+    SN_CT16B1->MR3 = 0xFF;
+
+    // Set prescale value
+    SN_CT16B1->PRE = 0x1F;
+
+    //Set CT16B1 as the up-counting mode.
+    SN_CT16B1->TMRCTRL = (mskCT16_CRST);
+
+    // Wait until timer reset done.
+    while (SN_CT16B1->TMRCTRL & mskCT16_CRST);
+
+    // Let TC start counting.
+    SN_CT16B1->TMRCTRL |= mskCT16_CEN_EN;
+
+    NVIC_ClearPendingIRQ(CT16B1_IRQn);
+    nvicEnableVector(CT16B1_IRQn, 4);
 }
 
 uint8_t matrix_scan(void) {
-    bool changed = false;
-
-#if defined(DIRECT_PINS) || (DIODE_DIRECTION == COL2ROW)
-    // Set row, read cols
-    for (uint8_t current_row = 0; current_row < MATRIX_ROWS; current_row++) {
-        changed |= read_cols_on_row(raw_matrix, current_row);
+    for (uint8_t row_index = 0; row_index < MATRIX_ROWS; row_index++) {
+        // Determine if the matrix changed state
+        if ((last_matrix[row_index] != raw_matrix[row_index])) {
+            matrix_changed         = true;
+            last_matrix[row_index] = raw_matrix[row_index];
+        }
     }
-#elif (DIODE_DIRECTION == ROW2COL)
-    // Set col, read rows
-    for (uint8_t current_col = 0; current_col < MATRIX_COLS; current_col++) {
-        changed |= read_rows_on_col(raw_matrix, current_col);
-    }
-#endif
 
-    debounce(raw_matrix, matrix, MATRIX_ROWS, changed);
+    debounce(raw_matrix, matrix, MATRIX_ROWS, matrix_changed);
 
     matrix_scan_quantum();
-    return (uint8_t)changed;
+
+    return matrix_changed;
+}
+
+uint8_t hw_row_to_matrix_row[18] = { 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 0 };
+/**
+ * @brief   MR1 interrupt handler.
+ *
+ * @isr
+ */
+OSAL_IRQ_HANDLER(Vector80) {
+
+    OSAL_IRQ_PROLOGUE();
+
+//    // Disable PWM outputs on column pins
+//    SN_CT16B1->PWMIOENB = 0;
+
+    SN_CT16B1->IC = mskCT16_MR3IC; // Clear match interrupt status
+    SN_CT16B1->TMRCTRL = CT16_CRST;
+
+//    // Turn the selected LED row off
+//    writePinLow(led_row_pins[current_led_row]);
+
+    // Enable current matrix row
+    select_col(current_col);
+
+
+    // Read the key matrix
+    for (uint8_t row_index = 0; row_index < MATRIX_ROWS; row_index++) {
+        // Check row pin state
+        if (readPin(row_pins[row_index]) == 0) {
+            // Pin LO, set col bit
+            raw_matrix[row_index] |= (MATRIX_ROW_SHIFTER << current_col);
+        } else {
+            // Pin HI, clear col bit
+            raw_matrix[row_index] &= ~(MATRIX_ROW_SHIFTER << current_col);
+        }
+    }
+
+    // Disable current matrix row
+    unselect_col(current_col);
+
+    // Turn the next row on
+//    current_led_row = (current_led_row + 1) % LED_MATRIX_ROWS_HW;
+
+    current_col = (current_col + 1) % MATRIX_COLS;
+
+//    uint8_t row_idx = hw_row_to_matrix_row[current_led_row];
+//    uint16_t row_ofst = row_ofsts[row_idx];
+//
+//    if(current_led_row % 3 == 0)
+//    {
+//        SN_CT16B1->MR8  = led_state[row_ofst + 0 ].b | 1;
+//        SN_CT16B1->MR9  = led_state[row_ofst + 1 ].b | 1;
+//        SN_CT16B1->MR10 = led_state[row_ofst + 2 ].b | 1;
+//        SN_CT16B1->MR11 = led_state[row_ofst + 3 ].b | 1;
+//        SN_CT16B1->MR12 = led_state[row_ofst + 4 ].b | 1;
+//        SN_CT16B1->MR13 = led_state[row_ofst + 5 ].b | 1;
+//        SN_CT16B1->MR14 = led_state[row_ofst + 6 ].b | 1;
+//        SN_CT16B1->MR15 = led_state[row_ofst + 7 ].b | 1;
+//        SN_CT16B1->MR16 = led_state[row_ofst + 8 ].b | 1;
+//        SN_CT16B1->MR17 = led_state[row_ofst + 9 ].b | 1;
+//        SN_CT16B1->MR18 = led_state[row_ofst + 10].b | 1;
+//        SN_CT16B1->MR19 = led_state[row_ofst + 11].b | 1;
+//        SN_CT16B1->MR20 = led_state[row_ofst + 12].b | 1;
+//        SN_CT16B1->MR21 = led_state[row_ofst + 13].b | 1;
+//        SN_CT16B1->MR22 = led_state[row_ofst + 14].b | 1;
+//        SN_CT16B1->MR23 = led_state[row_ofst + 15].b | 1;
+//        SN_CT16B1->MR0  = led_state[row_ofst + 16].b | 1;
+//        SN_CT16B1->MR1  = led_state[row_ofst + 17].b | 1;
+//        SN_CT16B1->MR2  = led_state[row_ofst + 18].b | 1;
+//    }
+//
+//    if(current_led_row % 3 == 1)
+//    {
+//        SN_CT16B1->MR8  = led_state[row_ofst + 0 ].g | 1;
+//        SN_CT16B1->MR9  = led_state[row_ofst + 1 ].g | 1;
+//        SN_CT16B1->MR10 = led_state[row_ofst + 2 ].g | 1;
+//        SN_CT16B1->MR11 = led_state[row_ofst + 3 ].g | 1;
+//        SN_CT16B1->MR12 = led_state[row_ofst + 4 ].g | 1;
+//        SN_CT16B1->MR13 = led_state[row_ofst + 5 ].g | 1;
+//        SN_CT16B1->MR14 = led_state[row_ofst + 6 ].g | 1;
+//        SN_CT16B1->MR15 = led_state[row_ofst + 7 ].g | 1;
+//        SN_CT16B1->MR16 = led_state[row_ofst + 8 ].g | 1;
+//        SN_CT16B1->MR17 = led_state[row_ofst + 9 ].g | 1;
+//        SN_CT16B1->MR18 = led_state[row_ofst + 10].g | 1;
+//        SN_CT16B1->MR19 = led_state[row_ofst + 11].g | 1;
+//        SN_CT16B1->MR20 = led_state[row_ofst + 12].g | 1;
+//        SN_CT16B1->MR21 = led_state[row_ofst + 13].g | 1;
+//        SN_CT16B1->MR22 = led_state[row_ofst + 14].g | 1;
+//        SN_CT16B1->MR23 = led_state[row_ofst + 15].g | 1;
+//        SN_CT16B1->MR0  = led_state[row_ofst + 16].g | 1;
+//        SN_CT16B1->MR1  = led_state[row_ofst + 17].g | 1;
+//        SN_CT16B1->MR2  = led_state[row_ofst + 18].g | 1;
+//    }
+//    if(current_led_row % 3 == 2)
+//    {
+//        SN_CT16B1->MR8  = led_state[row_ofst + 0 ].r | 1;
+//        SN_CT16B1->MR9  = led_state[row_ofst + 1 ].r | 1;
+//        SN_CT16B1->MR10 = led_state[row_ofst + 2 ].r | 1;
+//        SN_CT16B1->MR11 = led_state[row_ofst + 3 ].r | 1;
+//        SN_CT16B1->MR12 = led_state[row_ofst + 4 ].r | 1;
+//        SN_CT16B1->MR13 = led_state[row_ofst + 5 ].r | 1;
+//        SN_CT16B1->MR14 = led_state[row_ofst + 6 ].r | 1;
+//        SN_CT16B1->MR15 = led_state[row_ofst + 7 ].r | 1;
+//        SN_CT16B1->MR16 = led_state[row_ofst + 8 ].r | 1;
+//        SN_CT16B1->MR17 = led_state[row_ofst + 9 ].r | 1;
+//        SN_CT16B1->MR18 = led_state[row_ofst + 10].r | 1;
+//        SN_CT16B1->MR19 = led_state[row_ofst + 11].r | 1;
+//        SN_CT16B1->MR20 = led_state[row_ofst + 12].r | 1;
+//        SN_CT16B1->MR21 = led_state[row_ofst + 13].r | 1;
+//        SN_CT16B1->MR22 = led_state[row_ofst + 14].r | 1;
+//        SN_CT16B1->MR23 = led_state[row_ofst + 15].r | 1;
+//        SN_CT16B1->MR0  = led_state[row_ofst + 16].r | 1;
+//        SN_CT16B1->MR1  = led_state[row_ofst + 17].r | 1;
+//        SN_CT16B1->MR2  = led_state[row_ofst + 18].r | 1;
+//    }
+
+//    // Enable PWM outputs on column pins
+//    SN_CT16B1->PWMIOENB   = (mskCT16_PWM0EN_EN  \
+//                            |mskCT16_PWM1EN_EN  \
+//                            |mskCT16_PWM2EN_EN  \
+//                            |mskCT16_PWM8EN_EN  \
+//                            |mskCT16_PWM9EN_EN  \
+//                            |mskCT16_PWM10EN_EN \
+//                            |mskCT16_PWM11EN_EN \
+//                            |mskCT16_PWM12EN_EN \
+//                            |mskCT16_PWM13EN_EN \
+//                            |mskCT16_PWM14EN_EN \
+//                            |mskCT16_PWM15EN_EN \
+//                            |mskCT16_PWM16EN_EN \
+//                            |mskCT16_PWM17EN_EN \
+//                            |mskCT16_PWM18EN_EN \
+//                            |mskCT16_PWM19EN_EN \
+//                            |mskCT16_PWM20EN_EN \
+//                            |mskCT16_PWM21EN_EN \
+//                            |mskCT16_PWM22EN_EN \
+//                            |mskCT16_PWM23EN_EN);
+
+    SN_CT16B1->IC = SN_CT16B1->RIS;  // Clear all for now
+    SN_CT16B1->TMRCTRL = CT16_CEN_EN;
+
+//    writePinHigh(led_row_pins[current_led_row]);
+
+    OSAL_IRQ_EPILOGUE();
+}
+
+void HardFault_Handler(void) {
+    NVIC_SystemReset();
 }
