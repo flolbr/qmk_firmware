b79144ec9b81a1f6cf26efa5614814f8a31b363b
HorrorTroll <sonicvipduc@gmail.com>
2021-04-12 22:39:03 +0700

    oops, fixed

--- keyboards/redragon/k552/rev1/keymaps/horrortroll/effects/rainbow_reactive_simple.c
+++ keyboards/redragon/k552/rev1/keymaps/horrortroll/effects/rainbow_reactive_simple.c
@@ -1,7 +1,7 @@
 #include "effects/rainbow_reactive_simple/rainbow_reactive_simple.h"
 
 static HSV RAINBOW_REACTIVE_SIMPLE_math(HSV hsv, uint8_t i, uint8_t time, uint16_t offset) {
-	hsv.h = g_led_config.point[i].x - time;
+    hsv.h = g_led_config.point[i].x - time;
     hsv.v = scale8(255 - offset, hsv.v);
     return hsv;
 }
